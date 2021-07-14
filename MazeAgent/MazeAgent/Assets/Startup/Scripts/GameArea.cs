ec3990583970ad8c136dbf2183142f205dec4745
shammi <ssshammi@hotmail.com>
2021-07-14 09:40:19 +0100

    maze agent script added.

---
+++ MazeAgent/MazeAgent/Assets/Startup/Scripts/GameArea.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameArea : MonoBehaviour
+{
+    [Tooltip("The agent inside the area")]
+    public MazeAgent mazeAgent;
+
+
+
+    [Tooltip("The TextMeshPro text that shows the cumulative reward of the agent")]
+    public TextMeshPro cumulativeRewardText;
+
+    [Tooltip("Prefab of a live cheese")]
+    public Unity.MLAgentsExamples.TargetController cheesePrefab;
+
+    // [Tooltip("Prefab of the regurgitated cheese that appears when the chesee is rotten")]
+    //public GameObject regurgitatedcheesePrefab;
+
+    private List<GameObject> cheeseList;
+
+    /// <summary>
+    /// Reset the area, including cheese and mouse placement
+    /// </summary>
+    public void ResetArea()
+    {
+        RemoveAllcheese();
+        PlaceMouse();
+
+        Spawncheese(12, .5f);
+    }
+
+    /// <summary>
+    /// Remove a specific cheese from the area when it is eaten
+    /// </summary>
+    /// <param name="cheeseObject">The cheese to remove</param>
+    public void RemoveSpecificcheese(GameObject cheeseObject)
+    {
+        cheeseList.Remove(cheeseObject);
+        Destroy(cheeseObject);
+    }
+
+    /// <summary>
+    /// The number of cheese remaining
+    /// </summary>
+    public int cheeseRemaining
+    {
+        get { return cheeseList.Count; }
+    }
+
+    /// <summary>
+    /// Choose a random position on the X-Z plane within a partial donut shape
+    /// </summary>
+    /// <param name="center">The center of the donut</param>
+    /// <param name="minAngle">Minimum angle of the wedge</param>
+    /// <param name="maxAngle">Maximum angle of the wedge</param>
+    /// <param name="minRadius">Minimum distance from the center</param>
+    /// <param name="maxRadius">Maximum distance from the center</param>
+    /// <returns>A position falling within the specified region</returns>
+    public static Vector3 ChooseRandomPosition(Vector3 center, float minAngle, float maxAngle, float minRadius, float maxRadius)
+    {
+        float radius = minRadius;
+        float angle = minAngle;
+
+        if (maxRadius > minRadius)
+        {
+            // Pick a random radius
+            radius = UnityEngine.Random.Range(minRadius, maxRadius);
+        }
+
+        if (maxAngle > minAngle)
+        {
+            // Pick a random angle
+            angle = UnityEngine.Random.Range(minAngle, maxAngle);
+        }
+
+        // Center position + forward vector rotated around the Y axis by "angle" degrees, multiplies by "radius"
+        return center + Quaternion.Euler(0f, angle, 0f) * Vector3.forward * radius;
+    }
+
+    /// <summary>
+    /// Remove all cheese from the area
+    /// </summary>
+    private void RemoveAllcheese()
+    {
+        if (cheeseList != null)
+        {
+            for (int i = 0; i < cheeseList.Count; i++)
+            {
+                if (cheeseList[i] != null)
+                {
+                    Destroy(cheeseList[i]);
+                }
+            }
+        }
+
+        cheeseList = new List<GameObject>();
+    }
+
+    /// <summary>
+    /// Place the Mouse in the area
+    /// </summary>
+    private void PlaceMouse()
+    {
+        Rigidbody rigidbody = mazeAgent.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        mazeAgent.transform.position = new Vector3(6f,2.4f,12f);
+      //  mazeAgent.transform.position = ChooseRandomPosition(transform.position, 0f, 0f, 0f, 0f) ;
+       // mazeAgent.transform.rotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+    }
+
+
+
+    /// <summary>
+    /// Spawn some number of cheese in the area and set their swim speed
+    /// </summary>
+    /// <param name="count">The number to spawn</param>
+    /// <param name="cheeseSpeed">The swim speed</param>  .satic object
+    ///
+    private void Spawncheese(int count, float cheeseSpeed)
+    {
+        return;
+        for (int i = 0; i < count; i++)
+        {
+            // Spawn and place the cheese
+            GameObject cheeseObject = Instantiate<GameObject>(cheesePrefab.gameObject);
+            // add to random place on the maze
+            cheeseObject.transform.position = ChooseRandomPosition(transform.position, 100f, 260f, 2f, 13f) + Vector3.up * .5f;
+            cheeseObject.transform.rotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+
+            // Set the cheese's parent to this area's transform
+            cheeseObject.transform.SetParent(transform);
+
+            // Keep track of the cheese
+            cheeseList.Add(cheeseObject);
+
+            // Set the cheese decay time
+            //cheeseObject.GetComponent<cheese>().cheeseSpeed = cheeseSpeed;
+        }
+    }
+
+    /// <summary>
+    /// Called when the game starts
+    /// </summary>
+    private void Start()
+    {
+        //ResetArea();
+    }
+
+    /// <summary>
+    /// Called every frame
+    /// </summary>
+    private void Update()
+    {
+        // Update the cumulative reward text
+        cumulativeRewardText.text = mazeAgent.GetCumulativeReward().ToString("0.00");
+    }
+
+}
---
+++ MazeAgent/MazeAgent/Assets/Startup/Scripts/MazeAgent.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MazeAgent : Agent
+{
+    [Tooltip("How fast the agent moves forward")]
+    public float moveSpeed = 10f;
+
+    [Tooltip("How fast the agent turns")]
+    public float turnSpeed = 180f;
+
+    [Tooltip("Prefab of the heart that appears when the mouse is fed")]
+    public GameObject heartPrefab;
+
+
+
+    private GameArea gameArea;
+    new private Rigidbody rigidbody;
+
+    private bool isFull; // If true, mouse has a full stomach
+
+    /// <summary>
+    /// Initial setup, called when the agent is enabled
+    /// </summary>
+    public override void Initialize()
+    {
+        base.Initialize();
+        gameArea = GetComponentInParent<GameArea>();
+
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Perform actions based on a vector of numbers
+    /// </summary>
+    /// <param name="actionBuffers">The struct of actions to take</param>
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Convert the first action to forward movement
+        float forwardAmount = actionBuffers.DiscreteActions[0];
+
+        // Convert the second action to turning left or right
+        float turnAmount = 0f;
+        if (actionBuffers.DiscreteActions[1] == 1f)
+        {
+            turnAmount = -90f;
+        }
+        else if (actionBuffers.DiscreteActions[1] == 2f)
+        {
+            turnAmount = 90f;
+        }
+
+        // Apply movement
+        rigidbody.MovePosition(transform.position + transform.forward * forwardAmount * moveSpeed * Time.fixedDeltaTime);
+        transform.Rotate(transform.up * turnAmount  * Time.fixedDeltaTime);
+
+        // Apply a tiny negative reward every step to encourage action
+        if (MaxStep > 0) AddReward(-1f / MaxStep);
+    }
+
+    /// <summary>
+    /// Read inputs from the keyboard and convert them to a list of actions.
+    /// This is called only when the player wants to control the agent and has set
+    /// Behavior Type to "Heuristic Only" in the Behavior Parameters inspector.
+    /// </summary>
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        int forwardAction = 0;
+        int turnAction = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            // move forward
+            forwardAction = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // turn left
+            turnAction = 1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            // turn right
+            turnAction = 2;
+        }
+
+        // Put the actions into the array
+        actionsOut.DiscreteActions.Array[0] = forwardAction;
+        actionsOut.DiscreteActions.Array[1] = turnAction;
+    }
+
+    /// <summary>
+    /// When a new episode begins, reset the agent and area
+    /// </summary>
+    public override void OnEpisodeBegin()
+    {
+        isFull = false;
+        gameArea.ResetArea();
+    }
+
+    /// <summary>
+    /// Collect all non-Raycast observations
+    /// </summary>
+    /// <param name="sensor">The vector sensor to add observations to</param>
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Whether the mouse has eaten a cheese (1 float = 1 value)
+        sensor.AddObservation(isFull);
+
+
+
+
+        // Direction mouse is facing (1 Vector3 = 3 values)
+        sensor.AddObservation(transform.forward);
+
+        // 1 + 1 + 3 + 3 = 8 total values
+    }
+
+    /// <summary>
+    /// When the agent collides with something, take action
+    /// </summary>
+    /// <param name="collision">The collision info</param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("target"))
+        {
+            // Try to eat the cheese
+            Eatcheese(collision.gameObject);
+            CheckEnd();
+        }
+
+    }
+
+    /// <summary>
+    /// Check if agent is full, if not, eat the cheese and get a reward
+    /// </summary>
+    /// <param name="cheeseObject">The cheese to eat</param>
+    private void Eatcheese(GameObject cheeseObject)
+    {
+        if (isFull) return; // Can't eat another cheese while full
+       // isFull = true; // need to  define max hunger
+
+        gameArea.RemoveSpecificcheese(cheeseObject);
+
+        AddReward(1f);
+    }
+
+    /// <summary>
+    /// Check if agent is full, if yes, exit the maze
+    /// </summary>
+   /* private void Regurgitatecheese()
+    {
+        if (!isFull) return; // Nothing to regurgitate
+        isFull = false;
+
+
+        // Spawn regurgitated cheese
+        GameObject regurgitatedcheese = Instantiate<GameObject>(regurgitatedcheesePrefab);
+        regurgitatedcheese.transform.parent = transform.parent;
+        regurgitatedcheese.transform.position = transform.position;
+        Destroy(regurgitatedcheese, 4f);
+        AddReward(-1.0f);
+    }*/
+  private void CheckEnd()
+    {
+    if (!isFull) return; // Nothing to regurgitate
+  //      isFull = false;
+        // Spawn heart
+        GameObject heart = Instantiate<GameObject>(heartPrefab);
+        heart.transform.parent = transform;
+        heart.transform.position = transform.position + Vector3.up;
+        Destroy(heart, 4f);
+
+
+
+        if (gameArea.cheeseRemaining <= 0)
+        {
+            EndEpisode();
+        }
+    }
+}
